920cdfeed6b9810915a2cc841d5e0eae661fadcd
tuckerTU <tuckerishere@gmail.com>
2021-07-14 14:37:34 -0600

    commit tree 91c33bb636d35ac1bf7bf1f94d6ffe2161fb7de1

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  before_validation :make_title_case
+
+  before_save :email_author_about_post
+
   private
 
   def is_title_case
@@ -14,4 +18,13 @@ class Post < ActiveRecord::Base
   def make_title_case
     self.title = self.title.titlecase
   end
+
+   def email_author_about_post
+    # Not implemented.
+    # For more information: https://guides.rubyonrails.org/action_mailer_basics.html
+  end
+
+
+
+
 end
